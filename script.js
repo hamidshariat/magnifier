ed58e2957d41d3f5f9f7f4d2b35e7b44aeebe748
hamidshariat <hamid13841147@gmail.com>
2023-10-09 14:06:57 +0330

    Modify the code

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
-(function(window) {
-    let defineLibrary = () => ({
-        init : function(imageId) {
-            let image = document.querySelector(imageId);
-            if(! image ) {
-                console.error(`image element dosen't exist`);
-                return;
-            }
+(function (window) {
+  let defineLibrary = () => ({
+    init: function (imageId) {
+      let image = document.querySelector(imageId);
+      if (!image) {
+        console.error(`image element dosen't exist`);
+        return;
+      }
+      let zoomContainer, zoomResult, zoomLens;
+      image.addEventListener("mouseenter", function (e) {
+        let imageBox = this.getBoundingClientRect();
+        console.log(imageBox);
+        zoomContainer = document.createElement("div");
+        zoomContainer.classList.add("zoom-container");
+        zoomContainer.style.width = `${imageBox.width}px`;
+        zoomContainer.style.height = `${imageBox.height}px`;
+        zoomContainer.style.position = "absolute";
+        zoomContainer.style.top = `${imageBox.top + window.pageYOffset}px`;
+        zoomContainer.style.left = `${imageBox.left + window.pageXOffset}px`;
+        zoomLens = document.createElement("div");
+        zoomLens.classList.add("img-zoom-lens");
+        zoomResult = document.createElement("div");
+        zoomResult.classList.add("img-zoom-result");
+        zoomResult.style.position = "absolute";
+        zoomResult.style.top = "350px";
+        zoomContainer.insertAdjacentElement("afterbegin", zoomResult);
+        zoomContainer.insertAdjacentElement("afterbegin", zoomLens);
+        document
+          .querySelector("body")
+          .insertAdjacentElement("beforeend", zoomContainer);
+        let cx = zoomResult.offsetWidth / zoomLens.offsetWidth;
+        let cy = zoomResult.offsetHeight / zoomLens.offsetHeight;
+        console.log(cx, cy);
+        zoomResult.style.backgroundImage = ` url(${image.src})`;
+        zoomResult.style.backgroundSize = `${image.width * cx}px  ${
+          image.height * cy
+        }px`;
+        zoomContainer.addEventListener("mousemove", function (e) {
+          let x = e.clientX - imageBox.left;
+          let y = e.clientY + window.pageYOffset - imageBox.top;
+          x = x - zoomLens.offsetWidth / 2;
+          y = y - zoomLens.offsetHeight / 2;
+          if (x > image.width - zoomLens.offsetWidth) {
+            x = image.width - zoomLens.offsetWidth;
+          }
+          if (x < 0) {
+            x = 0;
+          }
+          if (y < 0) {
+            y = 0;
+          }
+          if (y > 165) {
+            y = 165;
+          }
+          zoomLens.style.top = `${y}px`;
+          zoomLens.style.left = `${x}px`;
+          zoomResult.style.backgroundPosition = `-${x * cx}px -${y * cy}px `;
+        });
+      });
+    },
+  });
 
-            let zoomContainer , zoomResult , zoomLens;
-
-            image.addEventListener('mouseenter' , function(e) {
-                
-                let imageBox = this.getBoundingClientRect();
-                console.log(imageBox)
-                zoomContainer = document.createElement('div');
-                zoomContainer.classList.add('zoom-container');
-                zoomContainer.style.width = `${imageBox.width}px`
-                zoomContainer.style.height = `${imageBox.height}px`
-                zoomContainer.style.position = 'absolute';
-                zoomContainer.style.top = `${imageBox.top + window.pageYOffset}px`
-                zoomContainer.style.left = `${imageBox.left + window.pageXOffset}px`
-                zoomLens = document.createElement('div')
-                zoomLens.classList.add('img-zoom-lens');
-                zoomResult = document.createElement('div');
-                zoomResult.classList.add('img-zoom-result')
-                zoomResult.style.position = 'absolute'
-                zoomResult.style.top = '350px'
-                zoomContainer.insertAdjacentElement('afterbegin' , zoomResult)
-                zoomContainer.insertAdjacentElement('afterbegin' , zoomLens)
-                document.querySelector('body').insertAdjacentElement('beforeend' , zoomContainer)
-                let cx = zoomResult.offsetWidth / zoomLens.offsetWidth
-                let cy = zoomResult.offsetHeight/ zoomLens.offsetHeight
-                console.log( cx , cy )
-                zoomResult.style.backgroundImage = ` url(${image.src})`
-                zoomResult.style.backgroundSize = `${image.width * cx}px  ${image.height * cy}px`
-                zoomContainer.addEventListener('mousemove' , function (e){
-                    let x = e.clientX - imageBox.left
-                    let y = ( e.clientY + window.pageYOffset) - imageBox.top
-                    x = x - (zoomLens.offsetWidth / 2)
-                    y = y - ( zoomLens.offsetHeight / 2)
-                    if ( x > image.width - zoomLens.offsetWidth ) { x = image.width - zoomLens.offsetWidth }; 
-                    if ( x<0 ){ x = 0 };
-                    if ( y<0 ) { y = 0} ;
-                    if ( y > 165) { y = 165 };
-
-                    zoomLens.style.top = `${y}px`;
-                    zoomLens.style.left = `${x}px`;
-                    zoomResult.style.backgroundPosition = `-${ x * cx}px -${ y * cy}px `
-                })
-            })
-
-        }
-    })
-
-    if(typeof(vanillaZoom) == 'undefined') {
-        window.vanillaZoom = defineLibrary();
-    } else {
-        console.log('library already defined.')
-    }
-})(window)
+  if (typeof vanillaZoom == "undefined") {
+    window.vanillaZoom = defineLibrary();
+  } else {
+    console.log("library already defined.");
+  }
+})(window);
