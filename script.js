9b11da7103837db486ed78d6513a57cdd935caa8
hamidshariat <hamid13841147@gmail.com>
2023-10-13 12:38:25 +0330

    Modify the code

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
   let defineLibrary = () => ({
     init: function (imageId) {
       let image = document.querySelector(imageId);
+
       if (!image) {
         console.error(`image element dosen't exist`);
         return;
       }
+
       let zoomContainer, zoomResult, zoomLens;
+
       image.addEventListener("mouseenter", function (e) {
         let imageBox = this.getBoundingClientRect();
         console.log(imageBox);
@@ -25,36 +28,42 @@
         zoomResult.style.top = "350px";
         zoomContainer.insertAdjacentElement("afterbegin", zoomResult);
         zoomContainer.insertAdjacentElement("afterbegin", zoomLens);
-        document
-          .querySelector("body")
-          .insertAdjacentElement("beforeend", zoomContainer);
-        let cx = zoomResult.offsetWidth / zoomLens.offsetWidth;
+
+        document.querySelector("body").insertAdjacentElement("beforeend", zoomContainer);
+
+          let cx = zoomResult.offsetWidth / zoomLens.offsetWidth;
         let cy = zoomResult.offsetHeight / zoomLens.offsetHeight;
-        console.log(cx, cy);
+
         zoomResult.style.backgroundImage = ` url(${image.src})`;
-        zoomResult.style.backgroundSize = `${image.width * cx}px  ${
-          image.height * cy
-        }px`;
+        
+        zoomResult.style.backgroundSize = `${image.width * cx}px  ${image.height * cy}px`;
+        
         zoomContainer.addEventListener("mousemove", function (e) {
           let x = e.clientX - imageBox.left;
           let y = e.clientY + window.pageYOffset - imageBox.top;
           x = x - zoomLens.offsetWidth / 2;
           y = y - zoomLens.offsetHeight / 2;
+          
           if (x > image.width - zoomLens.offsetWidth) {
             x = image.width - zoomLens.offsetWidth;
           }
+          
           if (x < 0) {
             x = 0;
           }
+          
           if (y < 0) {
             y = 0;
           }
+          
           if (y > 165) {
             y = 165;
           }
+          
           zoomLens.style.top = `${y}px`;
           zoomLens.style.left = `${x}px`;
           zoomResult.style.backgroundPosition = `-${x * cx}px -${y * cy}px `;
+       
         });
       });
     },
@@ -62,7 +71,5 @@
 
   if (typeof vanillaZoom == "undefined") {
     window.vanillaZoom = defineLibrary();
-  } else {
-    console.log("library already defined.");
   }
 })(window);
